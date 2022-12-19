ed4c4294965f6e7239638def881f4767768f08b6
Zepher Ashe <safesploit@users.noreply.github.com>
2022-12-19 17:06:25 +0000

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<form class="post_form" action="test.php" method="POST" enctype="multipart/form-data">
-			<input type="file" name="fileToUpload" id="fileToUpload"><br><br>
-			<input type="submit" name="post" id="post_button" value="Post">
-</form>
-
-<?php 
-include("includes/handlers/test.php");
-?>
